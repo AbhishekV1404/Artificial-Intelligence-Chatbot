c7c5bb8b4702a7db3cacba18d811ae41aca2a59c
Abhishek Verma <abhishekverma1404@gmail.com>
2024-01-26 11:14:40 +0530

    importing libraries

---
+++ training.py
@@ -0,0 +1,9 @@
+impoort randon
+import json
+import pickle
+import numpy as np
+import nktl
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensonflow.keras.models import Dense,Actication,Dropout
+from tensorflow.keras.optimizers import .SGD
