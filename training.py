849925d1fbcc7dc0b92a598e0221a6cf80005d64
Abhishek Verma <abhishekverma1404@gmail.com>
2024-01-26 12:08:40 +0530

    Loading Training data set

--- training.py
+++ training.py
@@ -1,9 +1,23 @@
-impoort randon
+import random
 import json
 import pickle
 import numpy as np
-import nktl
+import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import Sequential
-from tensonflow.keras.models import Dense,Actication,Dropout
-from tensorflow.keras.optimizers import .SGD
+from tensorflow.keras.layers import Dense,Activation,Dropout
+from tensorflow.keras.optimizers import SGD
+lemmatizer=WordNetLemmatizer()
+intents=json.loads(open('intents.json').read())
+words=[]
+classes=[]
+documents=[]
+ignore_letters=['?','.','!','.',',','_']
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list=nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list,intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+print(documents)
